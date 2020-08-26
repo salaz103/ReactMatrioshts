f700fa8cf83ee6bd64fd80d16463d93f25bce40e
salaz310 <lrso.10@hotmail.com>
2020-08-26 15:19:20 -0600

    Diseñando la UI

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import MatrioshtsApp from './components/MatrioshtsApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/theme-github";
+
 
 
 ReactDOM.render(<MatrioshtsApp/>,document.getElementById('app'));
--- src/components/MatrioshtsApp.js
+++ src/components/MatrioshtsApp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AceEditor from 'react-ace';
-import Action from './Traducir';
+import Action from './Action';
+import Header from './Header';
 import "ace-builds/src-noconflict/mode-typescript";
 import "ace-builds/src-noconflict/theme-twilight";
 import "ace-builds/src-noconflict/ext-language_tools"
@@ -21,19 +22,31 @@ export default class MatrioshtsApp extends React.Component {
       console.log(this.state.valor);
   }
 
+  ejecutar=()=>{
+      console.log('Presiono el boton de ejecutar');
+  }
+
   render() {
 
     return (
       <div>
+      <Header title='ReactMatrioshTS- 201213181'/>
+        <div className='container'>
         <AceEditor
-    onChange={this.onChange}
-    mode="typescript"
-    theme="twilight"
-    name="UNIQUE_ID_OF_DIV"
+            onChange={this.onChange}
+            mode="typescript"
+            theme="twilight"
+            name="UNIQUE_ID_OF_DIV"
         />
         <Action
-         traducir={this.traducir}   
+            action={this.traducir}
+            nombre='Traducir'
         />
+        <Action
+            action={this.ejecutar}
+            nombre= 'Ejecutar'
+        />
+        </div>
       </div>
     );
   }
